984fe144b34c298b5d34bea4d852d827bc79734d
amitkrshaw3-coder <amit.kr.shaw.3@gmail.com>
2026-06-23 15:09:58 +0530

    Update dpp_generator.py

--- dpp_generator.py
+++ dpp_generator.py
@@ -3,7 +3,7 @@ import requests
 import streamlit as st
 
 # Aapki key yahan daal di gayi hai
-MY_API_KEY = "AQ.Ab8RN6JuAh1uLkTtLpBYONwYTY0ctyts2M7OL3MucnsLke4nPA"
+MY_API_KEY = "API KEY"
 
 def generate_paathsala_dpp(subject, topic, target_class):
     # Seedha Google ke server ka direct link (Bina kisi library ke)
