019805fab31c33ce9fdbbeda00d4b2c994d09f07
amitkrshaw3-coder <amit.kr.shaw.3@gmail.com>
2026-06-24 04:52:09 +0530

    Update ai_bot.py

--- ai_bot.py
+++ ai_bot.py
@@ -1,1 +1,30 @@
+import streamlit as st
+import google.generativeai as genai
 
+def ask_paathsala_ai(question):
+
+    try:
+        genai.configure(
+            api_key=st.secrets["GEMINI_API_KEY"],
+            transport="rest"
+        )
+
+        model = genai.GenerativeModel("gemini-2.5-flash")
+
+        prompt = f"""
+        You are PAATHSALA AI Tutor.
+
+        Explain the student's doubt in simple language.
+
+        Student question:
+        {question}
+
+        Give step-by-step explanation.
+        """
+
+        response = model.generate_content(prompt)
+
+        return response.text
+
+    except Exception as e:
+        return f"⚠️ AI Error: {e}"
